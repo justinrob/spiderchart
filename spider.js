b482918339336f82cb70c9f8c5238a11e311020a
Justin <justin@neo.com>
2015-08-20 18:15:28 +0800

    MDL UI updates

--- spider.js
+++ spider.js
@@ -52,12 +52,16 @@ function change(e){
    e.preventDefault();
    var val1 = document.getElementById('value1').valueAsNumber;
    var val2 = document.getElementById('value2').valueAsNumber;   
-   var val3 = document.getElementById('value3').valueAsNumber; 
+   var val3 = document.getElementById('value3').valueAsNumber;
+   var val4 = document.getElementById('value4').valueAsNumber;   
+   var val5 = document.getElementById('value5').valueAsNumber; 
    var lab1 = document.getElementById('label1').value;
    var lab2 = document.getElementById('label2').value;   
-   var lab3 = document.getElementById('label3').value;   
-    var labels = [lab1, lab2, lab3]
-    var values = [val1, val2, val3]
+   var lab3 = document.getElementById('label3').value;
+   var lab4 = document.getElementById('label4').value;   
+   var lab5 = document.getElementById('label5').value;   
+    var labels = [lab1, lab2, lab3, lab4, lab5]
+    var values = [val1, val2, val3, val4, val5]
     var hue = 'green'
     var settings = makeSpiderChartSettings(labels, values, hue )    
     $('#container').highcharts(settings);
